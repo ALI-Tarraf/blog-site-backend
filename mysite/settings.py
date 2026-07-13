4deff50ae58abc1b292120e6e0058d0eb1bb4eab
Ali Tarraf <ali.tarraf@lit-co.net>
2026-07-13 09:55:48 +0300

    Update DATABASES in the settings to have fallback values so the build doesn't crash if a variable is missing

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME", "blog_db"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
